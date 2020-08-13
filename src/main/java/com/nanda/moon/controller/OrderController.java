fe8a7c17a420ebf0dfbddfb63d7b99361e24cacb
nanda <nandaceria@gmail.com>
2020-08-13 21:48:23 +0700

    Swagger

--- src/main/java/com/nanda/moon/controller/OrderController.java
+++ src/main/java/com/nanda/moon/controller/OrderController.java
@@ -10,7 +10,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nanda.moon.dto.OrderParamDTO;
+import com.nanda.moon.dto.ResponseDTO;
 import com.nanda.moon.service.OrderTransactionService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/order")
@@ -19,11 +26,17 @@ public class OrderController {
 	@Autowired
 	private OrderTransactionService orderTransactionService;
 
+	@Operation(summary = "Create Order")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Create Order", content = {
+			@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }) })
 	@RequestMapping(value = "/create", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> createOrder(@RequestBody OrderParamDTO orderParamDTO) {
 		return orderTransactionService.create(orderParamDTO);
 	}
 
+	@Operation(summary = "Get Order")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Get Order", content = {
+			@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }) })
 	@RequestMapping(value = "/get/{ordernumber}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> getOrder(@PathVariable("ordernumber") String ordernumber) {
 		return orderTransactionService.getOrder(ordernumber);
